13d7780b9ee25939840af3c819d294f19d867810
tarcisioalvesleop <tarcisioalvesleop@hotmail.com>
2023-02-15 20:03:13 -0300

    User insert

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -24,4 +24,9 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
+	//salvar no BD
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+	
 }
