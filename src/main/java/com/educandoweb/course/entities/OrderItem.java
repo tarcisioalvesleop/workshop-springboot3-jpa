ff113a841b3dba52974055f9844c8f899086603c
tarcisioalvesleop <tarcisioalvesleop@hotmail.com>
2023-02-15 16:09:40 -0300

    SubTotal & Total methods

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -64,6 +64,10 @@ public class OrderItem implements Serializable {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+	//para aparecer no Json inicia com get
+	public Double getSubTotal() {
+		return price * quantity;
+	}
 
 	@Override
 	public int hashCode() {
